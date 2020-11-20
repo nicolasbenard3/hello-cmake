6492ebecc1c0b6436f49af333ca37c4bc7cc1702
benardnicolas3 <67842599+nicolasbenard3@users.noreply.github.com>
2020-11-20 21:26:53 +0100

    Add files via upload

---
+++ hello Cmake.cpp
@@ -0,0 +1,12 @@
+﻿// hello Cmake.cpp : définit le point d'entrée de l'application.
+//
+
+#include "hello Cmake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
